36b0720ff36958f07d637f128f446c69e7353451
Ziv <95753868+Zivgl66@users.noreply.github.com>
2023-02-02 09:03:42 +0200

    Update connectToDB.js

--- server/connectToDB.js
+++ server/connectToDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-module.exports = mongoose.connect("mongodb+srv://Zivgl66:Aa123456@codeblock.ik5yn5k.mongodb.net/users", {
+module.exports = mongoose.connect("mongodb+srv://Zivgl66:Aa123456@codeblock.lk5yn5k.mongodb.net/users", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useUnifiedTopology: true,
