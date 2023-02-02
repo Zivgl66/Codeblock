476f4a7b5df44b096f179cf017a99c08d94f2879
Ziv <95753868+Zivgl66@users.noreply.github.com>
2023-02-02 08:13:58 +0200

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -4,18 +4,12 @@ export const initSocket = async () => {
   const options = {
     protocol: "wss",
     "force new connection": true,
+    'try multiple transports': true,
     forceNew: true,
     tryTransportsOnConnectTimeout: true,
     reconnectionAttempt: "Infinity",
     timeout: 10000,
-    transports: [
-      "websocket",
-      "polling",
-      "flashsocket",
-      "htmlfile",
-      "xhr-polling",
-      "jsonp-polling",
-    ],
+    transports: ['websocket', 'flashsocket', 'htmlfile', 'xhr-multipart', 'xhr-polling', 'jsonp-polling', 'polling'],
     allowEIO3: true,
     "polling duration": 10,
   };
