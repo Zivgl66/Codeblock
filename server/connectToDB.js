578d9a83e4db5fed52bfe6cbd40c794e1767c2e7
Ziv <95753868+Zivgl66@users.noreply.github.com>
2023-02-03 12:19:36 +0200

    Delete server directory

--- server/connectToDB.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-module.exports = mongoose.connect(process.env.MONGO_CONNECTION_STRING , {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useUnifiedTopology: true,
-});
